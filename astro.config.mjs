256e4edd33129075739cf250590a712d76b83048
jazicorn <40859840+jazicorn@users.noreply.github.com>
2025-03-09 03:53:45 -0400

    added cloudflare package

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://7th-heaven.jazicorn.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://7th-heaven.jazicorn.com',
+  integrations: [mdx(), sitemap()],
+  adapter: cloudflare(),
 });
