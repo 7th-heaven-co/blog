6bac2e8c874663ecf52f6e8fde744bac424ad9a5
jazicorn <40859840+jazicorn@users.noreply.github.com>
2025-03-09 07:20:50 -0400

    removed unused cloudflare pkg

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,8 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://7th-heaven.jazicorn.com',
   integrations: [mdx(), sitemap()],
-  adapter: cloudflare(),
 });
