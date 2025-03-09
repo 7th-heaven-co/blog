7f43cbf4b7e3ce1be2f3aed865fcf77a93069be8
jazicorn <40859840+jazicorn@users.noreply.github.com>
2025-03-09 03:28:29 -0400

    updated footer

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,6 @@
 
 export const SITE_TITLE = '7th Heaven';
 export const SITE_DESCRIPTION = 'Before the Fall';
+export const SITE = 'https://7th-heaven.jazicorn.com';
+export const PROFILE = 'https://github.com/jazicorn';
+export const AUTHOR = 'Jazicorn';
