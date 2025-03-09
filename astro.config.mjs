459ff15675390bf327f7825d993d25a7c49c1d4f
jazicorn <40859840+jazicorn@users.noreply.github.com>
2025-03-09 01:54:37 -0500

    updated site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://7th-heaven.jazicorn.com',
 	integrations: [mdx(), sitemap()],
 });
