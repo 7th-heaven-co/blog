cfc9aeb484111bef3d9566bb3b425955ec94e496
jazicorn <40859840+jazicorn@users.noreply.github.com>
2025-03-15 22:33:16 -0400

    created collections for newsletter categories

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
-# 7th Heaven Blog
+# [7th-Heaven.Blog](https://7th-heaven.jazicorn.com/)
 
 Blog all about [7th Heaven](https://7th-heaven.jazicorn.com/) by [Jazicorn](https://github.com/jazicorn) including most recent chapters
+
+**Tags**
+
+- releases
+- announcements
+- events
+- community-notes
+
+## [Content: Blog](./src/content/blog/)
+
+## [Content: Announcements](./src/content/announcements)
+
+## [Content: Events](./src/content/events)
+
+## [Content: Community Notes](./src/content/community_notes)
+
+## [Content: Newsletter](./src/content/newsletter)
+
+Subscribers can receives emails periodically about 7th-heaven depending on the blog category they subscribe to. This folder holds the emails sent for each category
 
 ## [Installation](./md/INSTALLATION.md)
 
--- src/content.config.ts
+++ src/content.config.ts
@@ -12,7 +12,57 @@ const blog = defineCollection({
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
+		// ['updates','...','...']
+		tags: z.array(z.string()), 
 	}),
 });
 
-export const collections = { blog };
+const announcements = defineCollection({
+	// Load Markdown and MDX files in the `src/content/announcements/` directory.
+	loader: glob({ base: './src/content/announcements', pattern: '**/*.{md,mdx}' }),
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+		// ['announcements','...','...']
+		tags: z.array(z.string()), 
+	}),
+});
+
+const community_notes = defineCollection({
+	// Load Markdown and MDX files in the `src/content/community_notes/` directory.
+	loader: glob({ base: './src/content/community_notes', pattern: '**/*.{md,mdx}' }),
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+		// ['community_notes','...','...']
+		tags: z.array(z.string()), 
+	}),
+});
+
+const events = defineCollection({
+	// Load Markdown and MDX files in the `src/content/events/` directory.
+	loader: glob({ base: './src/content/events', pattern: '**/*.{md,mdx}' }),
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+		// ['events','...','...']
+		tags: z.array(z.string()), 
+	}),
+});
+
+export const collections = { blog, announcements, community_notes, events };
