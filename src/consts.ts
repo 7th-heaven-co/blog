7b2ad1df4fc82f9bb85f0e89ad64ac8146d71d21
jazicorn <40859840+jazicorn@users.noreply.github.com>
2025-04-11 07:44:56 -0400

    updated url paths to const

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,17 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = '7th Heaven';
 export const SITE_DESCRIPTION = 'Before the Fall';
 export const SITE = 'https://7th-heaven.jazicorn.com';
 export const PROFILE = 'https://github.com/jazicorn';
 export const AUTHOR = 'Jazicorn';
+export const HOME = '/';
+export const BLOG = '/blog/1';
+export const NEWSLETTER = '/newsletter';
+export const ABOUT = '/about';
+export const COLLECTIONS = [
+    'announcements', 
+    'author', 
+    'community', 
+    'events', 
+    'heaven', 
+    'releases'
+];
